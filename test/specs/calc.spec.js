f97c05acc458b8351d784d5e6d7e1965928fb663
StephyStuff <stephanie.birch13@gmail.com>
2016-11-07 15:33:45 -0500

    Created success condidition for first function.

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -5,10 +5,20 @@ var expect = chai.expect;
 
 window.calc = window.calc || {};
 
-
+  /**
+   * [result description]
+   * @type {[type]}
+   */
   describe('my app', function() {
-    it('should return the sum of an array of numbers', function() {
-      expect([5, 4, 8]).to.equal([17]);
+    it('should add the numbers in an array and return the total of all numbers', function() {
+
+       var result = window.calc.sum([5, 4, 8]);
+        expect(result).to.equal(17);
+    });
+
+    it('shoud handle the function when no numbers are given', function(){
+        var result = window.calc.sum('balloons');
+        expect(result).to.not.be.a(num);
     });
   });
 
